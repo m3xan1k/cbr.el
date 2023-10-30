637c7411d43ca41891b5585b292d0ca154364678
m3xan1k <m3xan1k@duck.com>
2023-10-30 23:38:35 +0300

    packaging

--- cbr.el
+++ cbr.el
@@ -1,18 +1,37 @@
+;;; cbr.el --- Currency rates from cbr.ru  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023 Sergey Shevtsov
+
+;; Version: 0.1
+
+;; Author: Sergey Shevtsov <m3xan1k at duck.com>
+;; Created: 29 Oct 2023
+
+;; Keywords: finance
+;; URL: https://codeberg.org/m3xan1k/cbr.el
+
+;; Package-Requires: ((esxml "0.3.7"))
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software see <https://www.gnu.org/licenses/>.
+
+
 (require 'url)
 (require 'esxml-query)
 
-(defconst cbr-url "https://www.cbr.ru/currency_base/daily/")
-(defconst currency-codes ["AUD" "AZN" "AMD" "BYN" "BGN" "BRL"
-			  "HUF" "KRW" "VND" "HKD" "GEL" "DKK"
-			  "AED" "USD" "EUR" "EGP" "INR" "KZT"
-			  "CAD" "QAR" "KGS" "CNY" "MDL" "NZD"
-			  "TMT" "NOK" "PLN" "RON" "XDR" "RSD"
-			  "SGD" "TJS" "THB" "TRY" "UZS" "UAH"
-			  "GBP" "CZK" "SEK" "CHF" "ZAR" "JPY"])
+(defconst m3xan1k-cbr-url "https://www.cbr.ru/currency_base/daily/")
+(defconst m3xan1k-currency-codes ["AUD" "AZN" "AMD" "BYN" "BGN" "BRL"
+				  "HUF" "KRW" "VND" "HKD" "GEL" "DKK"
+				  "AED" "USD" "EUR" "EGP" "INR" "KZT"
+				  "CAD" "QAR" "KGS" "CNY" "MDL" "NZD"
+				  "TMT" "NOK" "PLN" "RON" "XDR" "RSD"
+				  "SGD" "TJS" "THB" "TRY" "UZS" "UAH"
+				  "GBP" "CZK" "SEK" "CHF" "ZAR" "JPY"])
 
 (defun m3xan1k-cbr--get-html ()
   "http request to cbr url and parse response body to esxml"
-  (with-current-buffer (url-retrieve-synchronously cbr-url)
+  (with-current-buffer (url-retrieve-synchronously m3xan1k-cbr-url)
     (set-buffer-multibyte t)
     (goto-char url-http-end-of-headers)
     (libxml-parse-html-region (point) (point-max))))
@@ -42,11 +61,11 @@ Return example (\"USD\" . 100.00)"
 	 (final-rate (/ (* currency-rate 1.0) currency-nominal)))
     (cons currency-code final-rate)))
 
-(defun m3xan1k-cbr-get-rate (target-code)
+(defun cbr-get-rate (target-code)
   "Prints currency rate in rub for given code.
 TARGET-CODE - on of currency-codes"
   (interactive "sCurrency code: ")
-  (if (not (seq-contains-p currency-codes (upcase target-code)))
+  (if (not (seq-contains-p m3xan1k-currency-codes (upcase target-code)))
       (print (format "No such code: %s" target-code))
     (let* ((html (m3xan1k-cbr--get-html))
 	   (rows (m3xan1k-cbr--html-to-rows html))
@@ -57,3 +76,6 @@ TARGET-CODE - on of currency-codes"
 	  (puthash (car code-rate-pair) (format "%.2f" (cdr code-rate-pair)) currency-map))
 	(setq rows (cdr rows)))
       (print (gethash (upcase target-code) currency-map)))))
+
+(provide 'cbr)
+;;; cbr.el ends here
